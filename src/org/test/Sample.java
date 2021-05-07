5f8c88e18664249f0232a5e890f6bfdcbd2b46aa
Subbukutty143 <subburajmariappan94@gmail.com>
2021-05-07 11:55:16 +0530

    2nd edit project

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -9,6 +9,11 @@ public class Sample{
 	public static void main(String[] args) {
 		List<Integer> l = new ArrayList<>();
 		boolean b = l.add(10);
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+		l.add(50);
 		
 		
 		
