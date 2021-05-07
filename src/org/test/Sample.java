daec2e659a1c3b11880f88d6687219876f250e84
Subbukutty143 <subburajmariappan94@gmail.com>
2021-05-07 11:30:53 +0530

    java

---
+++ src/org/test/Sample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample{
+	
+	
+	public static void main(String[] args) {
+		List<Integer> l = new ArrayList<>();
+		boolean b = l.add(10);
+		
+		
+		
+		
+	}
+
+}
